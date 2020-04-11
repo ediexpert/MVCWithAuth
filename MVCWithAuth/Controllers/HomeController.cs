b372f8a0d9c9ba35ebf2655c74f67b3cfac7b611
Imran Bajwa <imranbajwa01@gmail.com>
2020-04-12 00:28:28 +0200

    * .gitignore: initial commit
    * MVCWithAuth.sln:
    * app.db:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * MVCWithAuth.csproj:
    * Repository.cs:
    * site.js:
    * favicon.ico:
    * site.css:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * Error.cshtml:
    * Privacy.cshtml:
    * _ViewImports.cshtml:
    * _Layout.cshtml:
    * ApplicationDbContext.cs:
    * appsettings.Development.json:
    * LICENSE:
    * HomeController.cs:
    * LICENSE.txt:
    * launchSettings.json:
    * jquery.js:
    * _LoginPartial.cshtml:
    * jquery.min.js:
    * _ViewStart.cshtml:
    * jquery.min.map:
    * CartActionController.cs:
    * LICENSE.md:
    * 20200410225511_initial.cs:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.min.js:
    * bootstrap.js.map:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * 20200410225511_initial.Designer.cs:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * ApplicationDbContextModelSnapshot.cs:
    * bootstrap.min.css.map:
    * bootstrap.bundle.js.map:
    * jquery.validate.js:
    * bootstrap-grid.css.map:
    * LICENSE.txt:
    * bootstrap-grid.min.css:
    * bootstrap.bundle.min.js:
    * 00000000000000_CreateIdentitySchema.cs:
    * bootstrap-reboot.css.map:
    * bootstrap-reboot.min.css:
    * additional-methods.js:
    * bootstrap.bundle.min.js.map:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * 00000000000000_CreateIdentitySchema.Designer.cs:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ MVCWithAuth/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MVCWithAuth.Data;
+using MVCWithAuth.DomainObjects;
+using MVCWithAuth.Models;
+using NETCore.MailKit.Core;
+
+namespace MVCWithAuth.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IEmailService _emailService;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, IEmailService emailService, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _emailService = emailService;
+            _context = context;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            var tour = new Tour() { Name = "Test Tour" };
+            await _context.Set<Tour>().AddAsync(tour);
+            await _context.SaveChangesAsync();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCWithAuth/Data/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using MVCWithAuth.DomainObjects;
+
+namespace MVCWithAuth.Data
+{
+    public class Repository<T>:IRepository<T> where T:EntityBase
+    {
+        private ApplicationDbContext _dbContext;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return _dbContext.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> List()
+        {
+            return _dbContext.Set<T>();
+        }
+
+        public IEnumerable<T> List(Expression<Func<T, bool>> predicate)
+        {
+            return _dbContext.Set<T>()
+                .Where(predicate);
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _dbContext.SaveChanges();
+        }
+    }
+}
